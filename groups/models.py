0b432c791e5f8181e32b2400498be6dd4fc59536
lamar1524 <pawel.wybrzeze@gmail.com>
2020-03-25 18:31:21 +0100

    [T-2] Added serializers

--- groups/models.py
+++ groups/models.py
@@ -10,3 +10,8 @@ class Group(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     members = models.ManyToManyField(User, related_name='members')
     photo = models.ImageField(upload_to=MEDIA_ROOT)
+
+
+class PendingMember(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
