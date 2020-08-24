65a88eb876c78e549bca3068b518fbc9bf0d98f0
pmlynarski <pawel.wybrzeze@gmail.com>
2020-08-24 19:02:23 +0200

    Finished chat consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,8 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from chat.models import Message
+
 
 class ChatConsumer(WebsocketConsumer):
 
@@ -23,19 +25,30 @@ class ChatConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)(self.room_id, self.channel_name)
 
     def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
+        raw_data = [arg for arg in
+                    json.loads(text_data).replace('{', '').replace('}', '').replace(':', '').replace(',', '').split(
+                        "\"")
+                    if
+                    arg != '']
+        data_dict = {}
+        for i in range(len(raw_data)):
+            if i % 2 == 0 and i + 1 <= len(raw_data):
+                data_dict = {**data_dict, raw_data[i]: raw_data[i + 1]}
+        message = Message(thread_id=self.scope.get('url_route').get('kwargs').get('thread_id'),
+                          sender_id=data_dict.get('senderId'), content=data_dict.get('message'))
+        message.save()
         async_to_sync(self.channel_layer.group_send)(
             self.room_id,
             {
                 'type': 'chat_message',
-                'message': message
+                **data_dict
             }
         )
 
     def chat_message(self, event):
-        message = event['message']
+        message = event.get('message')
+        sender_id = event.get('senderId')
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'senderId': sender_id
         }))
